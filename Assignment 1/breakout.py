d97545927beebefd99e07d2d9b663d852a8c2079
OpusMag <90897827+OpusMag@users.noreply.github.com>
2022-02-18 14:27:33 +0100

    added comments to breakout.py

--- Assignment 1/breakout.py
+++ Assignment 1/breakout.py
@@ -1,7 +1,7 @@
 from pygame import Vector2
 import pygame
 
-
+#edited pre code variables to fit with the variables I have used
 def intersect_paddle_ball(rec_pos, sx, sy,
                                ball_pos, ball_radius, ball_speed):
     """ Determine if a rectangle and a circle intersects.
@@ -133,12 +133,16 @@ def breakout():
     clock = pygame.time.Clock()
     pygame.display.update()
 
+    #defining variables
+
     #brick colors used
     brick_red = (255, 0, 0)
     brick_green = (0, 255, 0)
     brick_blue = (0, 0, 255)
+    
     #paddle color
     paddle_col = (255, 255, 255)
+    
     #ball color
     ball_col = (192, 192, 192)
 
@@ -147,27 +151,55 @@ def breakout():
     brick_green_num = 20
     brick_blue_num = 20
 
-    #
+    #defining red brick variables
+    brick_red_size_x = 20
+    brick_red_size_y = 10
+    brick_red_pos_x = 0
+    brick_red_pos_y = 0
 
+    #defining green brick variables
+    brick_green_size_x = 20
+    brick_green_size_y = 10
+    brick_green_pos_x = 0
+    brick_green_pos_y = 10
+
+    #defining blue brick variables
+    brick_blue_size_x = 20
+    brick_blue_size_y = 10
+    brick_blue_pos_x = 0
+    brick_blue_pos_y = 20
+
+    #defining paddle variables
+    paddle_radius = 10
+    paddle_pos_x = pygame.mouse.get_pos()
+    paddle_pos_y = 590
+
+    #defining ball variables
+    ball_radius = 5
+    ball_pos = Vector2(400, 300)
+    ball_speed = Vector2(2, -2)
+    
+    #class for the red bricks
     class Brick_red:
         def __init__(self, brick_red_pos_x, brick_red_pos_y, brick_red_size_x, brick_red_size_y):
             self.brick_red_size_x = brick_red_size_x
             self.brick_red_size_y = brick_red_size_y
             self.brick_red_pos_x = brick_red_pos_x
-            self.brick_red_pos_y = brick_red_pos_y
+            self.brick_red_pos_y = brick_red_pos_y  
         
         def draw(self):
             rect = pygame.rect(self, brick_red_pos_x, brick_red_pos_y, brick_red_size_x, brick_red_size_y)
             for brick_red in range(brick_red_num):
                 pygame.draw.rect(screen, (255, 0, 0),
                 (brick_red_pos_x * 2, brick_red_pos_y * 2, brick_red_size_x, brick_red_size_y))
-
+    
+    #class for the green bricks
     class Brick_green:
         def __init__(self, brick_green_pos_x, brick_green_pos_y, brick_green_size_x, brick_green_size_y):
             self.brick_green_size_x = brick_green_size_x
             self.brick_green_size_y = brick_green_size_y
             self.brick_green_pos_x = brick_green_pos_x
-            self.brick_green_pos_y = brick_green_pos_y
+            self.brick_green_pos_y = brick_green_pos_y 
         
         def draw(self):
             rect = pygame.rect(self, brick_green_pos_x, brick_green_pos_y, brick_green_size_x, brick_green_size_y)
@@ -175,12 +207,13 @@ def breakout():
                 pygame.draw.rect(screen, (255, 0, 0),
                 (brick_green_pos_x * 2, brick_green_pos_y * 2, brick_green_size_x, brick_green_size_y))
 
+    #class for the blue bricks
     class Brick_blue:
         def __init__(self, brick_blue_pos_x, brick_blue_pos_y, brick_blue_size_x, brick_blue_size_y):
             self.brick_blue_size_x = brick_blue_size_x
             self.brick_blue_size_y = brick_blue_size_y
             self.brick_blue_pos_x = brick_blue_pos_x
-            self.brick_blue_pos_y = brick_blue_pos_y
+            self.brick_blue_pos_y = brick_blue_pos_y  
     
         def draw(self):
             rect = pygame.rect(self, brick_blue_pos_x, brick_blue_pos_y, brick_blue_size_x, brick_blue_size_y)
@@ -188,69 +221,74 @@ def breakout():
                 pygame.draw.rect(screen, (255, 0, 0),
                 (brick_blue_pos_x * 2, brick_blue_pos_y * 2, brick_blue_size_x, brick_blue_size_y))
 
+    #class and methods for the paddle
     class Paddle():
         def __init__(self):
-            self.paddle_radius = 10
-            self.paddle_pos_x = pygame.mouse.get_pos()
-            self.paddle_pos_y = 590
+            self.paddle_radius = paddle_radius 
+            self.paddle_pos_x = paddle_pos_x
+            self.paddle_pos_y = paddle_pos_y  
         
         def move(self):
-            self.paddle_pos_x = pygame.mouse.get_pos()
-            self.paddle_pos_y = 590
+            self.paddle_pos_x = paddle_pos_x
+            self.paddle_pos_y = paddle_pos_y
 
         def draw(self):
             circle = pygame.circle(self, paddle_radius, paddle_pos_x, paddle_pos_y)
             pygame.draw.circle(screen, (255, 255, 255), (paddle_radius, paddle_pos_x, paddle_pos_y))
 
-
+    #class and methods for the ball
     class Ball():
         def __init__(self):
-            self.ball_radius = 5
-            self.ball_pos = Vector2(x, y)
+            self.ball_radius = ball_radius
+            self.ball_pos = ball_pos 
 
         def move(self):
-            self.ball_speed = Vector2(2, -2)
-    
-        def draw(self):
-            circle = pygame.circle(self, ball_radius, ball_pos_x, ball_pos_y, ball_speed_x, ball_speed_y)
-            pygame.draw.rect(screen, (192, 192, 192), (ball_radius, ball_pos_x, ball_pos_y, ball_speed_x, ball_speed_y))
+            self.ball_speed = ball_speed
 
+        def draw(self):
+            circle = pygame.circle(self, ball_radius, ball_pos, ball_speed)
+            pygame.draw.rect(screen, (192, 192, 192), (ball_radius, ball_pos, ball_speed))
+
+        #tests and handles intersections between paddle and ball
         impulse = intersect_paddle_ball(paddle_pos_x, 
                                         paddle_pos_y, 
                                         paddle_radius,
-                                        ball_position,
+                                        ball_pos,
                                         ball_radius,
                                         ball_speed)
         if impulse:
             ball_speed = ball_speed * -1
 
-        impulse = intersect_brick_red_ball(brick_red_position_x,
-                                            brick_red_position_y,
+        #tests and handles intersections between red bricks and ball
+        impulse = intersect_brick_red_ball(brick_red_pos_x,
+                                            brick_red_pos_y,
                                             brick_red_size_x,
                                             brick_red_size_y,
-                                            ball_position,
+                                            ball_pos,
                                             ball_radius,
                                             ball_speed)
         if impulse:
             ball_speed = ball_speed * -1
             del brick_red
 
-        impulse = intersect_brick_green_ball(brick_green_position_x,
-                                            brick_green_position_y,
+        #tests and handles intersections between green bricks and ball
+        impulse = intersect_brick_green_ball(brick_green_pos_x,
+                                            brick_green_pos_y,
                                             brick_green_size_x,
                                             brick_green_size_y,
-                                            ball_position,
+                                            ball_pos,
                                             ball_radius,
                                             ball_speed)
         if impulse:
             ball_speed = ball_speed * -1
             del brick_green
         
-        impulse = intersect_brick_blue_ball(brick_blue_position_x,
-                                            brick_blue_position_y,
+        #tests and handles intersections between blue bricks and ball
+        impulse = intersect_brick_blue_ball(brick_blue_pos_x,
+                                            brick_blue_pos_y,
                                             brick_blue_size_x,
                                             brick_blue_size_y,
-                                            ball_position,
+                                            ball_pos,
                                             ball_radius,
                                             ball_speed)
         if impulse:
