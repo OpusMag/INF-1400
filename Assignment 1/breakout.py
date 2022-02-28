d06b65997e8faa441ba259ab20725d2f90f30aa8
OpusMag <90897827+OpusMag@users.noreply.github.com>
2022-02-28 15:28:58 +0100

    Runs, but ball doesn't move

--- Assignment 1/breakout.py
+++ Assignment 1/breakout.py
@@ -74,31 +74,32 @@ def intersect_brick_ball(brick_position_x,
 
 class Breakout:
 
-    
-    #brick_ob.draw_bricks()
-
     def game_loop(self):
         pygame.init()
         screen = pygame.display.set_mode((800, 600), 0, 32)
         pygame.display.set_caption('Breakout')
         clock = pygame.time.Clock()
         rows = 3
-        columns = 19
+        columns = 22
         
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+            #lag objekter
             brick_ob = Brick()
+            ball_ob = Ball()
+            paddle_ob = Paddle()
+            #flytt objekter
+            paddle_ob.move_paddle()
+            ball_ob.move_ball()
+            #tegn objekter
             brick_ob.create_bricks(rows, columns)
             brick_ob.draw_bricks(screen)
-            ball_ob = Ball()
             ball_ob.draw_ball(screen)
-            ball_ob.move_ball()
-            paddle_ob = Paddle()
-            #paddle_ob.draw_paddle(screen)
-            paddle_ob.move_paddle(screen)
+            paddle_ob.draw_paddle(screen)
+            
             pygame.display.update()
             clock.tick(60)
         pygame.quit()
@@ -156,28 +157,36 @@ class Paddle:
         self.paddle_radius = 20
         self.paddle_speed = [10, 10]
             
-    def move_paddle(self, screen):
+    def move_paddle(self):
         paddle_pos = pygame.mouse.get_pos()
         self.paddle_pos_x = paddle_pos[0]
-        self.paddle_pos_y = paddle_pos[1]
-        pygame.draw.circle(screen, (255, 255, 255), (round(self.paddle_pos_x), round(self.paddle_pos_y)), self.paddle_radius)
-        pygame.display.update()
+        self.paddle_pos_y = 600
+        #pygame.display.update()
 
-    #def draw_paddle(self, screen):
+    def draw_paddle(self, screen):
         #circle = pygame.circle(self.paddle_radius, self.paddle_pos_x, self.paddle_pos_y)
         #pygame.draw.circle(screen, (255, 255, 255), (round(self.paddle_pos_x), round(self.paddle_pos_y)), self.paddle_radius)
+        pygame.draw.circle(screen, (255, 255, 255), (int(self.paddle_pos_x), int(self.paddle_pos_y)), self.paddle_radius)
 
 #class and methods for the ball
 class Ball:
     def __init__(self):
         self.ball_pos_x = 400
         self.ball_pos_y = 300
+        self.ball_speed = [10, 10]
         self.ball_radius = 10
-        self.ball_speed = [20, 20]
 
     def move_ball(self):
         self.ball_pos_x += self.ball_speed[0]
         self.ball_pos_y += self.ball_speed[1]
+        
+    
+    def draw_ball(self, screen):
+        #pygame.draw.crcle((screen), (192, 192, 192), (self.ball_pos_x, self.ball_pos_y), self.ball_radius)
+        pygame.draw.circle(screen, (192, 192, 192),
+                           (int(self.ball_pos_x),
+                           int(self.ball_pos_y)),
+                           self.ball_radius)
         
     def collision_paddle(impulse, paddle_pos_x, paddle_pos_y, paddle_radius, ball_pos, ball_radius, ball_speed):
         #tests and handles intersections between paddle and ball
@@ -203,10 +212,7 @@ class Ball:
             ball_speed = ball_speed * -1
             del brick
 
-    def draw_ball(self, screen):
-        self.screen = screen
-        #circle = pygame.Circle(self.ball_radius, self.ball_pos, self.ball_speed)
-        pygame.draw.circle((screen), (192, 192, 192), (self.ball_pos_x, self.ball_pos_y), self.ball_radius)
+    
 
 if __name__ == '__main__':
     br = Breakout()
