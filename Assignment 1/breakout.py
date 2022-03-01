d559daf3d14049d2166894323c159a4fbe6f11e5
OpusMag <90897827+OpusMag@users.noreply.github.com>
2022-03-01 13:30:33 +0100

    Trying to get collision detection to work

--- Assignment 1/breakout.py
+++ Assignment 1/breakout.py
@@ -1,87 +1,50 @@
 from pygame import Vector2
 import pygame
+   
+    #my implementation of breakout  
+class Breakout:
 
-#edited pre code variables to fit with the variables I have used
-def intersect_paddle_ball(rec_pos, sx, sy,
-                               ball_pos, ball_radius, ball_speed):
-    """ Determine if a rectangle and a circle intersects.
-    Only works for a rectangle aligned with the axes.
-    Parameters:
-    rec_pos     - A Vector2 representing the position of the rectangles upper,
-                  left corner.
-    sx          - Width of rectangle.
-    sy          - Height of rectangle.
-    circle_pos  - A Vector2 representing the circle's position.
-    circle_radius - The circle's radius.
-    circle_speed - A Vector2 representing the circles speed.
-    Returns:
-    None if no intersection. 
-    If the rectangle and the circle intersect,returns a 
-    normalized Vector2 pointing in the direction the circle will
-    move after the collision.
-    """
+    
 
-    # Position of the walls relative to the ball
-    top = (rec_pos.y) - ball_pos.y
-    bottom = (rec_pos.y + sy) - ball_pos.y
-    left = (rec_pos.x) - ball_pos.x
-    right = (rec_pos.x + sx) - ball_pos.x
-
-    r = ball_radius
-    intersecting = left <= r and top <= r and right >= -r and bottom >= -r
-
-    if intersecting:
-        # Now need to figure out the vector to return.
-        impulse = ball_speed.normalize()
-
-        if abs(left) <= r and impulse.x > 0:
-            impulse.x = -impulse.x
-        if abs(right) <= r and impulse.x < 0:
-            impulse.x = -impulse.x
-        if abs(top) <= r and impulse.y > 0:
-            impulse.y = -impulse.y
-        if abs(bottom) <= r and impulse.y < 0:
-            impulse.y = -impulse.y
-        return impulse.normalize()
-    return None
-
-
-def intersect_brick_ball(brick_position_x, 
-                            brick_position_y, 
-                            brick_size_x, 
-                            brick_size_y, 
-                            ball_pos, ball_radius):
-    """ Determine if the red bricks and the ball intersects
-    Parameters:
-    brick_position_x       - Brick's position on the x axis
-    brick_position_y       - Brick's position on the y axis
-    brick_size_x           - Brick's size on the x axis
-    brick_size_y           - Brick's size on the y axis
-    ball_position              - A Vector2D representing the ball's position
-    ball_radius                - Ball's radius
-    Returns:
-    None if no intersection.
-    If the ball intersects with a red brick, it returns a normalized
-    Vector2 pointing from brick_red to ball.
-    """
-    # vector from A to B
-    dp1p2 = ball_pos - (brick_position_x + brick_position_y)
-
-    if brick_size_x + brick_size_y + ball_radius >= pygame.math.Vector2.length(dp1p2):
-        return dp1p2.normalize()
-    else:
-        return None
-
- 
+    def game_loop(self):
+        pygame.init()
+        screen = pygame.display.set_mode((800, 600), 0, 32)
+        pygame.display.set_caption('Breakout')
+        clock = pygame.time.Clock()
+        time_passed = clock.tick(30) / 1000.0
+        rows = 3
+        columns = 22
+        brick_ob = Brick()
+        ball_ob = Ball()
+        paddle_ob = Paddle()
+        
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
             
-    #my implementation of breakout        
+            #flytt objekter
+            paddle_ob.move_paddle()
+            ball_ob.move_ball()
+            #tegn objekter
+            paddle_ob.draw_paddle(screen)
+            brick_ob.create_bricks(rows, columns)
+            brick_ob.draw_bricks(screen)
+            ball_ob.draw_ball(screen)
+            
+            pygame.display.flip()
+            pygame.display.update()
+            screen.fill((0,0,0))
+            clock.tick(60)
+        pygame.quit()
+        quit()         
     
 #class for the bricks
 class Brick:
 
     def __init__(self):
-        self.brick_pos_x = 1
-        self.brick_pos_y = 1
+        self.brick_pos = Vector2(1, 1)
         self.brick_size_x = 40
         self.brick_size_y = 20
         self.brick_red_col = 255, 0, 0
@@ -95,9 +58,9 @@ class Brick:
         for rows in range(rows):
             brick_row = []
             for columns in range(columns):
-                self.brick_pos_x = columns * self.brick_size_x
-                self.brick_pos_y = rows * self.brick_size_y
-                rect = pygame.Rect(self.brick_pos_x, self.brick_pos_y, self.brick_size_x, self.brick_size_y)
+                self.brick_pos.x = columns * self.brick_size_x
+                self.brick_pos.y = rows * self.brick_size_y
+                rect = pygame.Rect(self.brick_pos.x, self.brick_pos.y, self.brick_size_x, self.brick_size_y)
                 if rows == 0:
                     brick_col = 255, 0, 0
                 elif rows == 1:
@@ -128,7 +91,7 @@ class Paddle:
     def draw_paddle(self, screen):
         #circle = pygame.circle(self.paddle_radius, self.paddle_pos_x, self.paddle_pos_y)
         #pygame.draw.circle(screen, (255, 255, 255), (round(self.paddle_pos_x), round(self.paddle_pos_y)), self.paddle_radius)
-        pygame.draw.circle(screen, (255, 255, 255), (int(self.paddle_pos.x), int(self.paddle_pos.y)), self.paddle_radius)
+        pygame.draw.circle(screen, (255, 255, 255), (round(self.paddle_pos.x), round(self.paddle_pos.y)), self.paddle_radius)
             
     def move_paddle(self):
         paddle_pos = pygame.mouse.get_pos()
@@ -145,14 +108,82 @@ class Ball:
     def __init__(self):
         self.ball_pos = Vector2(400, 300)
         self.ball_radius = 5
-        self.ball_speed = Vector2(30, 30)
+        self.ball_speed = Vector2(500, 500).normalize()
     
     def draw_ball(self, screen):
         #pygame.draw.crcle((screen), (192, 192, 192), (self.ball_pos_x, self.ball_pos_y), self.ball_radius)
-        pygame.draw.circle(screen, (192, 192, 192), (int(self.ball_pos.x), int(self.ball_pos.y)), self.ball_radius, 0)
+        pygame.draw.circle(screen, (192, 192, 192), (round(self.ball_pos.x), round(self.ball_pos.y)), self.ball_radius, 0)
         
-    def move_ball(self, time_passed):
-        self.ball_pos += self.ball_speed * time_passed
+    def move_ball(self):
+        self.ball_pos += self.ball_speed
+
+    #edited pre code variables to fit with the variables I have used
+    def intersect_brick_ball(brick_pos, brick_size_x, brick_size_y,
+                               ball_pos, ball_radius, ball_speed):
+        """ Determine if a rectangle and a circle intersects.
+        Only works for a rectangle aligned with the axes.
+        Parameters:
+        rec_pos     - A Vector2 representing the position of the rectangles upper,left corner.
+        sx          - Width of rectangle.
+        sy          - Height of rectangle.
+        circle_pos  - A Vector2 representing the circle's position.
+        circle_radius - The circle's radius.
+        circle_speed - A Vector2 representing the circles speed.
+        Returns:
+        None if no intersection. 
+        If the rectangle and the circle intersect,returns a 
+        normalized Vector2 pointing in the direction the circle will
+        move after the collision.
+        """
+
+        # Position of the walls relative to the ball
+        top = (brick_pos.y) - ball_pos.y
+        bottom = (brick_pos.y + brick_size_y) - ball_pos.y
+        left = (brick_pos.x) - ball_pos.x
+        right = (brick_pos.x + brick_size_x) - ball_pos.x
+
+        r = ball_radius
+        intersecting = left <= r and top <= r and right >= -r and bottom >= -r
+
+        if intersecting:
+            # Now need to figure out the vector to return.
+            impulse = ball_speed.normalize()
+
+            if abs(left) <= r and impulse.x > 0:
+                impulse.x = -impulse.x
+            if abs(right) <= r and impulse.x < 0:
+                impulse.x = -impulse.x
+            if abs(top) <= r and impulse.y > 0:
+                impulse.y = -impulse.y
+            if abs(bottom) <= r and impulse.y < 0:
+                impulse.y = -impulse.y
+            return impulse.normalize()
+        return None
+
+
+    def intersect_paddle_ball(ball_pos, 
+                            ball_radius, 
+                            paddle_pos, 
+                            paddle_radius):
+        """ Determine if the red bricks and the ball intersects
+        Parameters:
+        paddle_pos             - Paddle's position on the x axis
+        paddle_radius          - Paddle's size on the x axis
+        brick_size_y           - Brick's size on the y axis
+        ball_position          - A Vector2D representing the ball's position
+        ball_radius            - Ball's radius
+        Returns:
+        None if no intersection.
+        If the ball intersects with a red brick, it returns a normalized
+        Vector2 pointing from brick_red to ball.
+        """
+        # vector from A to B
+        dp1p2 = ball_pos - paddle_pos
+
+        if paddle_radius + ball_radius >= pygame.math.Vector2.length(dp1p2):
+            return dp1p2.normalize()
+        else:
+            return None
         
         """if self.ball_pos.x < 0:
             self.ball_speed = abs(Vector2(0, 30))
@@ -162,19 +193,13 @@ class Ball:
             self.ball_speed = abs(Vector2(0, 30))
         if self.ball_pos.y > 600:
             self.ball_speed = abs(Vector2(30, 0))"""
-        
-    def collision_paddle(impulse, paddle_pos_x, paddle_pos_y, paddle_radius, ball_pos, ball_radius, ball_speed):
-        #tests and handles intersections between paddle and ball
-        impulse = intersect_paddle_ball(paddle_pos_x, 
-                                    paddle_pos_y, 
-                                    paddle_radius,
-                                    ball_pos,
-                                    ball_radius,
-                                    ball_speed)
+    def collision_paddle_ball(ball_pos, ball_radius, paddle_pos, paddle_radius):
+        impulse = intersect_paddle_ball(ball_pos, ball_radius,
+                                    paddle_pos, paddle_radius)
         if impulse:
             ball_speed = ball_speed * -1
 
-    def collision_brick(brick_pos_x, brick_pos_y, brick_size_x, brick_size_y, ball_pos, ball_radius, ball_speed):
+    def collision_brick_ball(brick_pos_x, brick_pos_y, brick_size_x, brick_size_y, ball_pos, ball_radius, ball_speed):
         #tests and handles intersections between bricks and ball
         impulse = intersect_brick_ball(brick_pos_x,
                                         brick_pos_y,
@@ -185,43 +210,7 @@ class Ball:
                                         ball_speed)
         if impulse:
             ball_speed = ball_speed * -1
-            del brick
-
-class Breakout:
-
-    def game_loop(self):
-        pygame.init()
-        screen = pygame.display.set_mode((800, 600), 0, 32)
-        pygame.display.set_caption('Breakout')
-        clock = pygame.time.Clock()
-        time_passed = clock.tick(30) / 1000.0
-        rows = 3
-        columns = 22
-        brick_ob = Brick()
-        ball_ob = Ball()
-        paddle_ob = Paddle()
-        
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-            
-            #flytt objekter
-            paddle_ob.move_paddle()
-            ball_ob.move_ball(time_passed)
-            #tegn objekter
-            paddle_ob.draw_paddle(screen)
-            brick_ob.create_bricks(rows, columns)
-            brick_ob.draw_bricks(screen)
-            ball_ob.draw_ball(screen)
-            
-            pygame.display.flip()
-            pygame.display.update()
-            screen.fill((0,0,0))
-            clock.tick(60)
-        pygame.quit()
-        quit()   
+            kill(Brick)
 
 if __name__ == '__main__':
     br = Breakout()
